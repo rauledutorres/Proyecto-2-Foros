7f840c8e8b907e1bc84e3233d0bb4d163d4cf4ea
KevsCha <k-e-v-i-n_13_@hotmail.com>
2022-07-27 12:24:38 +0200

    1

---
+++ login-registro.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login/Sign up</title>
+</head>
+<body>
+        <div class="container">
+            <div class="present cont">   
+                <div class="slider">
+
+                </div>
+            </div>
+            <div class="form cont">
+                <div class="caja_trasera">
+                    <span class="form_header btn_left">Registrarse</span>
+                    <span class="form_header btn_rigth">Iniciar Sesion</span>
+                    <div class="caja login">
+                        <form method="post" action="">
+                            <input type="text" placeholder="Email" />
+                            <input type="password" placeholder="Password"/>
+                            <button>Iniciar Sesion</button>
+                        </form>
+                    </div>
+                    <div class="caja registro">
+                        <form method="post" action="">
+                            <input type="text" placeholder="Nick_Name">
+                            <input type="text" placeholder="Email">
+                            <input type="text" placeholder="Password">
+                            <input type="file" accept="image/jpg" accept="image/png" >
+                            <button>Registrarse</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+</body>
+</html>
