7fa0aaf60448de6b7301d7ce1a2a8ffdbaa6ea58
adrianmoratilla <amoratillariofrio@gmail.com>
2022-07-28 18:13:26 +0200

    Script botones e inicio de responsive

--- editProfile.php
+++ editProfile.php
@@ -1,46 +1,80 @@
 <link rel="stylesheet" href="./css/editProfile.css">
-<div id="container">
-    <div id="userProfile">
-        <img class="profilePic" src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" width="150px" alt="profile picture">
-        <form>
-            <div class="image-upload">
-                <label for="profilePic">
-                    <img src="./img/icons/camera.svg" width="35px" height="35px">
-                </label>
-                <input type="file" id="profilePic" name="profilePic">
-            </div>
-            <div class="inputBox">
-                <img src="./img/icons/user.svg" width="20px" height="20x" class="icon" id="userIcon">
-                <img src="./img/icons/edit.svg" width="20px" height="20x" class="icon" id="editIcon">
-                <input type="text" name="nombre" id="userName" placeholder="Nuevo nombre de usuario" value="Prueba" readonly>
-            </div>
-            <div>
-                <img src="./img/icons/lock.svg" width="20px" height="20x" class="icon" id="passIcon">
-                <input type="password" placeholder="Nueva contraseña">
-            </div>
-            <div>
-                <img src="./img/icons/lock.svg" width="20px" height="20x" class="icon" id="passConfirmIcon">
-                <input type="password" placeholder="Confirma la contraseña">
-            </div>
-            <button class="button">Guardar</button>
-        </form>
-    </div>
-    <div id="userThread">
-        <div id="title">
-            <img src="./img/icons/question.svg" width="25px" height="25px" class="icon" id="questionIcon">
-            <h2>Mis hilos</h2>
+
+<body>
+
+    <div id="container">
+        <div id="userProfile">
+            <img class="profilePic" src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" width="150px" alt="profile picture">
+            <form>
+                <div class="image-upload">
+                    <label for="profilePic">
+                        <img src="./img/icons/camera.svg" width="35px" height="35px">
+                    </label>
+                    <input type="file" id="profilePic" name="profilePic">
+                </div>
+                <div class="inputBox">
+                    <img src="./img/icons/user.svg" class="icon" id="userIcon">
+                    <img src="./img/icons/edit.svg" class="icon editIcon" id="editUser">
+                    <input type="text" name="nombre" id="userName" placeholder="Nuevo nombre de usuario" value="Prueba" readonly>
+                </div>
+                <div class="inputBox">
+                    <img src="./img/icons/lock.svg" class="icon" id="passIcon">
+                    <img src="./img/icons/edit.svg" class="icon editIcon" id="editPass">
+                    <input type="password" id="actualPass" placeholder="Contraseña actual" value="holaque" readonly>
+                </div>
+                <div id="passContainer">
+                    <div class="inputBox">
+                        <img src="./img/icons/lock.svg" class="icon" id="passIcon">
+                        <input type="password" placeholder="Nueva contraseña">
+                    </div>
+                    <div class="inputBox">
+                        <img src="./img/icons/lock.svg" class="icon" id="passConfirmIcon">
+                        <input type="password" placeholder="Confirma la contraseña">
+                    </div>
+                </div>
+                <div id="buttonContainer">
+                    <button type="reset" class="button cancel" id="cancelButton">Cancelar</button>
+                    <button type="submit" class="button">Guardar</button>
+                </div>
+            </form>
         </div>
-        <div id="threadContent">
-            <img src="./img/web/man-thinking.svg" alt="man thinking">
-            <p>¡No seas tímido, publica algo!</p>
+        <div id="userThread">
+            <div id="title">
+                <img src="./img/icons/question.svg" class="icon" id="questionIcon">
+                <h2>Mis hilos</h2>
+            </div>
+            <div id="threadContent">
+                <img src="./img/web/man-thinking.svg" alt="man thinking">
+                <p>¡No seas tímido, publica algo!</p>
+            </div>
         </div>
     </div>
-</div>
+</body>
 <script>
-    var editIcon = document.getElementById('editIcon')
-    editIcon.onclick = function() {
-        document.getElementById('userName').removeAttribute('readonly');
+    var editUser = document.getElementById('editUser');
+    editUser.onclick = function() {
+        document.getElementById('userName').readOnly = false;
         document.getElementById('userName').value = "";
-        editIcon.style.cursor = "auto";
+        document.getElementById('buttonContainer').style.display = "block";
+        editUser.style.cursor = auto;
+    }
+
+    var editPass = document.getElementById('editPass');
+    editPass.onclick = function() {
+        document.getElementById("actualPass").value = "";
+        document.getElementById('actualPass').readOnly = false;
+        document.getElementById("passContainer").style.display = "block";
+        document.getElementById('buttonContainer').style.display = "block";
+    }
+
+    document.getElementById('profilePic').onclick = function() {
+        document.getElementById('buttonContainer').style.display = "block";
+    }
+
+    document.getElementById('cancelButton').onclick = function() {
+        document.getElementById('userName').readOnly = true;
+        document.getElementById('actualPass').readOnly = true;
+        document.getElementById("passContainer").style.display = "none";
+        document.getElementById('buttonContainer').style.display = "none";
     }
 </script>
