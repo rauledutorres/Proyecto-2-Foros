25a45ca0dba0cc43a729bf1cd367e7bdad506ae2
Raúl Torres <99418611+rauledutorres@users.noreply.github.com>
2022-07-29 14:03:47 +0200

    creacion pagina hilo

--- paginaHilos.php
+++ paginaHilos.php
@@ -12,11 +12,45 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/paginaHilos.css">
+    
 </head>
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <?php include './components/headerBien.php' ?>
-
-    
+        <div class="paginaEntera">
+            <div class="temasLista">
+                <div class="tituloLista">
+                    <h4>Temas</h4>
+                </div>
+                <ul>
+                    <button><li><img src="./img/CircleWavyQuestion.png">Ciencia</li></button>
+                    <button><li><img src="./img/Compass.png">Musica</li></button>
+                    <button><li><img src="./img/Compass.png">Sociales</li></button>
+                    <button><li><img src="./img/Compass.png">Deportes</li></button>
+                    <button><li><img src="./img/Compass.png">Videojuegos</li></button>
+                </ul>
+            </div>
+            
+            <div class="listaHilos">
+                <h2>Ciencias</h2>
+                <div class="hilo">
+                    <div class="hiloFoto">
+                        <img src="./img/woman1.png" alt="">
+                        <h5>ariana95</h5>
+                    </div>
+                    <div class="hiloTexto">
+                        <div class="hiloTitulo">
+                            <h3>Lanzamiento Telescopio James Webb</h3>
+                        </div>
+                        <div class="hiloTime">
+                            <h6>vie, 29 de Julio del 2022, 13:31:35(GMT)</h6>
+                        </div>
+                        <div class="hiloDesc">
+                            <p>Después de innumerables retrasos, el 25/12/2021 a las 12:20 UTC parece que va a ser la fecha definitiva para el lanzamiento del telescopio espacial James Webb, el telescopio mas potente y complejo de la historia que se espera revolucione el conocimiento en la astrofísica.</p>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>    
 </body>
 </html>
