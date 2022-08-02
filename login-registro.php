216ffde9dffc86e6aad36ad7c14a7eb602f5f1de
KevsCha <k-e-v-i-n_13_@hotmail.com>
2022-08-02 15:29:18 +0200

    1

--- login-registro.php
+++ login-registro.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,35 +12,64 @@
 </head>
 <body>
         <div class="container">
-            <div class="present cont">   
-                <div class="slider">
-
+            <div class="header">
+                <div class="header_cont">
+                    <img src="img/icono.png" alt=""/>
+                    <div>
+                        <h1><i>Foro</i></h1>
+                        <h4>Habla con internet</h4>
+                    </div>
+                </div>   
+                <div id="is" class="btn is">
+                    <span>Iniciar Sesion</span>
+                </div>
+                <div id="reg" class="btn">
+                    <span>Registrarse</span>
                 </div>
             </div>
-            <div class="form cont">
-                <div class="caja_trasera">
-                    <span class="form_header btn_left">Registrarse</span>
-                    <span class="form_header btn_rigth">Iniciar Sesion</span>
-                    <div class="caja login">
-                        <form method="post" action="">
-                            <input type="text" placeholder="Email" />
-                            <input type="password" placeholder="Password"/>
-                            <button>Iniciar Sesion</button>
+            <main>
+                <div id="login" class="caja">
+                    <div class="cont_form log">
+                        <div class="title_log">
+                            <h2>Accede a Foro</h2>
+                            <h4>La Mayor comunidad de foros</h4>
+                        </div>
+                        <form  action="">
+                            <label class="img_group img_log_name" ><img src="img/icon_input_nom.png" alt=""></label>
+                            <input class="inp_log" type="text" placeholder="Email" />
+                            <label class="img_group img_log_pass" ><img src="img/icon_input_pass.png" alt=""></label>
+                            <input class="inp_log" type="password" placeholder="Password"/>
+                            <a class="form_te" href="">¿Has olvidado tu contraseña?</a>
+                            <button class="btn_log" >Login</button>
                         </form>
                     </div>
-                    <div class="caja registro">
-                        <form method="post" action="">
-                            <input type="text" placeholder="Nick_Name">
-                            <input type="text" placeholder="Email">
-                            <input type="text" placeholder="Password">
-                            <input type="file" accept="image/jpg" accept="image/png" >
-                            <button>Registrarse</button>
-                        </form>
+                    <div class="cont_img">
+                        <img class="img_one" src="img/img_log.png" alt="">
                     </div>
-
                 </div>
-            </div>
+
+                <div id="registro" class="caja registro">
+                    <div class="cont_img">
+                        <img class="img_two" src="img/image_log2.png" alt="">
+                    </div>
+                    <div class="cont_form reg">
+                        <div class="title_reg">
+                            <h2>Unete a Foro</h2>
+                            <h4>La Mayor comunidad de foros</h4>
+                        </div>
+                        <form  action="">
+                            <input class="inp_reg" type="text" placeholder="Nick_Name">
+                            <input class="inp_reg" type="text" placeholder="Email">
+                            <input class="inp_reg" type="text" placeholder="Password">
+                            <input class="inp_reg" type="text" placeholder="Repite tu password">
+                            <button class="btn_reg">Registrate</button>
+                            <span>¿Ya tienes Cuenta?<a class="form_et" href="">Inicia Sesion</a></span>
+                        </form>
+                        
+                    </div> 
+                </div>
+            </main>
         </div>
-        
+        <script src="scrip.js"></script>
 </body>
 </html>
